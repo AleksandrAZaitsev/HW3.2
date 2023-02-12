c0da6c0bbcac72cfc7210a01ab2c4c5be95c6d0e
Aleksandr <zaytsev.chel@gmail.com>
2023-02-12 18:55:41 +0500

    add HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+if (number < 0)
+{
+   number = -number; 
+}
+
+if (number > 99 && number < 1000)
+{
+    int center = number / 10 % 10;
+    Console.WriteLine(center);
+}
+
